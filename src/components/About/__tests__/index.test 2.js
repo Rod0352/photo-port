415608fe02214e51c4f429aa4b71242a62fc06d1
Rod0352 <92071426+Rod0352@users.noreply.github.com>
2022-04-01 20:54:27 -0400

    nav bar and gallery added, lifted components in App.js

---
+++ src/components/About/__tests__/index.test 2.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {render, cleanup} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+// This will ensure that after each test, we won't have any leftover memory data that could give us false results.
+afterEach(cleanup);
+
+describe('About component', () => {
+    // First Test
+    // we use the it function. In the first argument, a string declares what is being tested. In the second argument, a callback function runs the test.
+    it('renders', () => {
+      render(<About />);
+    });
+  
+    // Second Test
+  })
+  
